9950d9f484c9adbf2339f64be1829439ab9292db
vochihieu <vo.chi.hieu@sun-asterisk.com>
2021-12-14 10:03:38 +0700

    Show product

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,5 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
-  end
+  def help; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home", to: "static_pages#home"
   end
-  get "static_pages/home", to: "static_pages#home"
+
 end
